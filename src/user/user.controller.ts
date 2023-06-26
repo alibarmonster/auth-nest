685ec6d3463ea2851aaf5cc2c5d9775769d50d37
alibarmonster <alibar838@gmail.com>
2023-06-26 20:01:47 +0700

    refactor: code for register

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,16 +1,4 @@
-import { Controller, Post, Body, UsePipes } from '@nestjs/common';
-import { UserService } from './user.service';
-import { CreateUserDto } from './dto';
-import { JoiValidationPipe } from 'src/custom-validation/joiValidationPIpe';
-import { createUserJoiSchema } from 'src/schema';
+import { Controller } from '@nestjs/common';
 
 @Controller('user')
-export class UserController {
-  constructor(private readonly userService: UserService) {}
-
-  @Post()
-  @UsePipes(new JoiValidationPipe(createUserJoiSchema))
-  createUser(@Body() createUserDto: CreateUserDto) {
-    return this.userService.createUser(createUserDto);
-  }
-}
+export class UserController {}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,18 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { User } from '../entities';
-import { Repository } from 'typeorm';
-import { CreateUserDto } from './dto/user-dto';
 
 @Injectable()
-export class UserService {
-  constructor(
-    @InjectRepository(User) private readonly userRepository: Repository<User>,
-  ) {}
-
-  async createUser(userDto: CreateUserDto) {
-    const user = this.userRepository.create(userDto);
-
-    return await this.userRepository.save(user);
-  }
-}
+export class UserService {}
